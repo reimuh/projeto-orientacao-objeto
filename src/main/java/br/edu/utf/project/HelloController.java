f5f4ebf325a59633d2c79597b882529527a1a963
OtavioGonzaga <otaviolgonzaga@gmail.com>
2025-09-11 21:33:41 -0300

    initial commit

---
+++ src/main/java/br/edu/utf/project/HelloController.java
@@ -0,0 +1,13 @@
+package br.edu.utf.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "Aplicação está rodando 🚀";
+    }
+}
---
+++ src/test/java/br/edu/utf/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utf.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
