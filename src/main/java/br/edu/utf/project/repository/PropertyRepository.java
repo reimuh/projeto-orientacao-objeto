0a913166c6f415fb7c1e6292f233360ddf10883b
OtavioGonzaga <otaviolgonzaga@gmail.com>
2025-10-16 21:40:42 -0300

    style: format property repository

--- src/main/java/br/edu/utf/project/repository/PropertyRepository.java
+++ src/main/java/br/edu/utf/project/repository/PropertyRepository.java
@@ -2,8 +2,10 @@ package br.edu.utf.project.repository;
 
 import java.util.List;
 import java.util.UUID;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import br.edu.utf.project.model.PropertyModel;
 
 @Repository
